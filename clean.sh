64fea0947611b35ffc53d42cc00c801840c334bc
Span0 <Span0@users.noreply.github.com>
2019-06-19 14:14:58 +0000

    Wed Jun 19 14:14:58 UTC 2019

--- clean.sh
+++ clean.sh
@@ -9,7 +9,6 @@ for f in in/* ; do
 	sed -i '/192.168.0.0\/16/d' $f
 	sed -i '/10.0.0.0\/8/d' $f
 	sed -i '/172.16.0.0\/12/d' $f
-	sed -i '/0.0.0.0\/8/d' $f
 	sed -i '/224.0.0.0\/3/d' $f
 	sed -i '/213.230.210.230/d' $f
 	mv $f blacklist/
